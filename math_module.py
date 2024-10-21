28cc71cab63832b67c2a5913319ede78b6c94b32
devarsh2007 <sunilpatel2131@gmail.com>
2024-10-21 19:54:23 +0530

    module

---
+++ math_module.py
@@ -0,0 +1,12 @@
+from math import *
+
+print(ceil(5.6))
+print(ceil(3.1))
+print(ceil(4.0))
+print(ceil(-2.1))
+
+print(floor(3.9))
+print(floor(2.1))
+print(floor(-2.3))
+
+print(floor(fabs(-3.5)))
---
+++ module2.py
@@ -0,0 +1,25 @@
+from calc import greet
+from calc import *
+
+greet()
+
+obj = calculator(1,5)
+obj.add()
+
+a=10
+def hello():
+    a=20
+    print(a)
+    
+hello()
+
+
+import calc  
+print(dir(calc))
+
+import math
+print(dir(math))
+l1 = dir(math)
+
+# for i in l1:
+#     print((i))
