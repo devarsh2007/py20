26d1b907825a913d5c0f5287e08fe5020286685d
devarsh2007 <sunilpatel2131@gmail.com>
2024-10-24 20:06:06 +0530

    package

--- mypackage/password_module.py
+++ mypackage/password_module.py
@@ -5,18 +5,22 @@ lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',  'n', 
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 symbol = ['!', '"', '#', '$', '%', '&', "'" , '(', ')', '*', '+', ',', '-',  '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^',  '_', '`', '{', '|', '}', '~']
-
+name = "devarsh"
 from random import *
+# find pass word from given length
+length = int(input("enter length : "))
+a = int(length/4)
+if length>=4:
+    pass1 = choices(upper,k=a)
+    pass2 = choices(lower,k=a)
+    pass3 = choices(number,k=a)
+    pass4 = choices(symbol,k=a)
+    password = pass1+pass2+pass3+pass4
+    print(password)
 
-pass1 = choices(upper,k=4)
-
-pass2 = choices(lower,k=4)
-pass3 = choices(number,k=4)
-pass4 = choices(symbol,k=4)
-
-password = pass1+pass2+pass3+pass4
-print(password)
-
-shuffle(password)
-print(password)
-print((len("".join(password))))
+    shuffle(password)
+    print(password)
+    print((("".join(password))))
+    
+else:
+    print("minimum required is 4")
