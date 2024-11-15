874ee6253057402785c802ae365e518dbacf5874
devarsh2007 <sunilpatel2131@gmail.com>
2024-11-15 19:02:50 +0530

    sql

--- project.py
+++ project.py
@@ -1,19 +1,35 @@
 import mysql.connector as con
 import seller
 
-db = con.connect(host='localhost',password='',user='root',database = 'project20')
-
-print("connection established...")
-
-print("welcome to product management")
-
 print("1 for seller view\n2 for customer view")
 choise = int(input("enter your choise : "))
 
 if choise==1:
     print("----------------- seller view --------------------")
+    exit=0
+    while exit==0:
+        print("\n1 for add\n2 for remove\n3 for update\n4 for fetch products\n5 for exit")
+        ch = int(input("enter your choise : "))
+        
+        if ch==1:
+            seller.add()
+            
+        elif ch==2:
+            seller.remove()
+            
+        elif ch==3:
+            seller.update()
+            
+        elif ch==4:
+            seller.select()
+            
+        elif ch==5:
+            exit=1
+            print("exited.....")
+            
+        else:
+            print("choise not found")
 
-    
 elif choise==2:
     print("-------------------- customer view --------------------")
     
--- seller.py
+++ seller.py
@@ -42,3 +42,56 @@ def update():
     if update==1:
         name = input("enter product name : ")
         sql = "update products set name=%s where id=%s"
+        Values=[name,id]
+        
+    elif update == 2:
+        desc = input("enter product description : ")
+        sql = "update products set description=%s where id=%s"
+        Values=[desc,id]
+        
+    elif update==3:
+        price = int(input("enter product price : "))
+        sql = "update products set price=%s where id=%s"
+        Values=[price,id]
+        
+    elif update==4:
+        isavailable = int(input("enter product status : "))
+        sql = "update products set isavailable=%s where id=%s"
+        Values=[isavailable,id]
+        
+    elif update==5:
+        name = input("enter product name : ")
+        desc = input("enter product description : ")
+        price = int(input("enter product price : "))
+        isavailable = int(input("enter product status : "))
+        sql = "update products set name=%s,description=%s,price=%s,isavailable=%s where id=%s"
+        Values=[name,desc,price,isavailable,id]
+        
+    else:
+        print("choise not found")
+        
+    mycursor = db.cursor()
+    mycursor.execute(sql,Values)
+    print("data updated successfuly")
+    db.commit()
+    
+    
+def select():
+    sql = "select * from products"
+    mycursor = db.cursor()
+    mycursor.execute(sql)
+    
+    data = mycursor.fetchall()
+    # print(data)
+    
+    # print("id   name    description     price       isavailable")'
+    # print("dy",dh)
+    # print(f"dy {dh}")
+
+    l1 = ["id","name","description","price","isavailable"]
+    print(f"{l1[0]:5} {l1[1]:10} {l1[2]:45}  {l1[3]:10} {l1[4]:10}")
+    print("-"*85)
+
+    for i in data:
+        print(f"{i[0]:2}    {i[1]:10} {i[2]:40} {i[3]:10} {i[4]:10}")
+    
